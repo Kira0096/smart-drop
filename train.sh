38d5eb02be91c64111bf932159ec853e0183e3aa
OYZH <578218819@qq.com>
2018-11-26 12:10:19 +0800

    gitignore update and config update

--- train.sh
+++ train.sh
@@ -1,2 +1,3 @@
-echo $*
-GLOG_vmodule=MemcachedClient=-1 srun --mpi=pmi2 -p $1 -n$2 --gres=gpu:$2 --ntasks-per-node=$2 python resnet-cifar10.py  --device 0 $3 $4 $5 $6 $7
+#echo $*
+#GLOG_vmodule=MemcachedClient=-1 srun --mpi=pmi2 -p $1 -n$2 --gres=gpu:$2 --ntasks-per-node=$2 python resnet-cifar10.py  --device 0 $3 $4 $5 $6 $7
+rlaunch --cpu=2 --gpu=1 --memory=4096 --preemptible=yes -- python3 resnet-cifar10.py -c config.yml
